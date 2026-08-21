6e1a87de0a2181a6569724eb2fe5fc1c4a4ebad4
Ali <alidev0110@gmail.com>
2026-08-21 22:05:02 +0300

    feat: add strict to login & create user schema to reject addition fileds

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -1,29 +1,33 @@
 import { z } from "zod";
 
-const createUserSchema = z.object({
-  name: z
-    .string()
-    .min(2, "Name must be more than 2 letters")
-    .max(20, "Name must be less than 20 letters"),
-  email: z.string().trim().toLowerCase().pipe(z.email()),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters")
-    .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
-    .regex(/[0-9]/, "Password must contain at least one number")
-    .regex(
-      /[^a-zA-Z0-9]/,
-      "Password must contain at least one special character",
-    ),
-  phone: z.string().min(8, "Phone number seems too short").optional(),
-});
+const createUserSchema = z
+  .object({
+    name: z
+      .string()
+      .min(2, "Name must be more than 2 letters")
+      .max(20, "Name must be less than 20 letters"),
+    email: z.string().trim().toLowerCase().pipe(z.email()),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(
+        /[^a-zA-Z0-9]/,
+        "Password must contain at least one special character",
+      ),
+    phone: z.string().min(8, "Phone number seems too short").optional(),
+  })
+  .strict();
 
 type CreateUserInput = z.infer<typeof createUserSchema>;
 
-const loginUserSchema = z.object({
-  email: z.string().trim().toLowerCase().pipe(z.email()),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-});
+const loginUserSchema = z
+  .object({
+    email: z.string().trim().toLowerCase().pipe(z.email()),
+    password: z.string().min(8, "Password must be at least 8 characters"),
+  })
+  .strict();
 
 type LoginUserInput = z.infer<typeof loginUserSchema>;
 
