3a11f00156cafccd816810dca7775c57183f51fd
Ali <alidev0110@gmail.com>
2026-08-21 21:51:30 +0300

    fix: correct email norlization order in validation schemas

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -5,7 +5,7 @@ const createUserSchema = z.object({
     .string()
     .min(2, "Name must be more than 2 letters")
     .max(20, "Name must be less than 20 letters"),
-  email: z.email().lowercase().trim(),
+  email: z.string().trim().toLowerCase().pipe(z.email()),
   password: z
     .string()
     .min(8, "Password must be at least 8 characters")
@@ -21,7 +21,7 @@ const createUserSchema = z.object({
 type CreateUserInput = z.infer<typeof createUserSchema>;
 
 const loginUserSchema = z.object({
-  email: z.email().lowercase().trim(),
+  email: z.string().trim().toLowerCase().pipe(z.email()),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
 
